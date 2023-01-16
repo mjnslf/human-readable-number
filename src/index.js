2cd67252859312fe6150dfd37443908900380f06
mjnslf <frecklessmadina@gmail.com>
2023-01-16 17:33:12 +0600

    feat: add two digits numer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 module.exports = function toReadable (number) {
     let one_dig_arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let two_dig_arr = ['ten', 'elven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen', 'nineteen'];
     if (number < 10){
         return one_dig_arr[number];
+    }else if(number >= 10 && number <= 19){
+        return two_dig_arr[number-10];
     }
 }
