11d63a9a5d587c5643f0936a5298ecfdffe4ae61
mjnslf <frecklessmadina@gmail.com>
2023-01-16 17:18:38 +0600

    feat: works for one digit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+    let one_dig_arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    if (number < 10){
+        return one_dig_arr[number];
+    }
 }
